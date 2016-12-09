0e9c00e70ea17b02fa49553e56008ce0fdc6b21e
tedoreve <wlzzs666@gmail.com>
2016-12-09 17:03:25 +0800

    auto commit

--- zmf.py
+++ zmf.py
@@ -29,7 +29,10 @@ def conversion(v,bmaj,bmin):
 def plot_fits(data,head,contrast,name):
     '''
     plot original continuum figure
+    contrast is the contrast ratio, it would be better to use 1.
+    name is the title of the image
     '''
+    
     w = wcs.WCS(head)
     if head['NAXIS'] == 2:
         l1,b1 = w.wcs_pix2world(0,0,0)
@@ -41,12 +44,48 @@ def plot_fits(data,head,contrast,name):
         l1,b1,v,s = w.wcs_pix2world(0,0,0,0,0)
         l2,b2,v,s = w.wcs_pix2world(data.shape[1],data.shape[0],0,0,0)
 
-    
     result = np.nan_to_num(data)
     result = (result-np.mean(result))+np.mean(result)*contrast
 
     plt.subplots() 
     plt.title(name)
-    plt.imshow(np.log(result),origin='lower',interpolation='nearest',extent=[l2,l1,b1,b2])
+    plt.imshow(np.log(result),origin='lower',interpolation='nearest',extent=[l1,l2,b1,b2])
     plt.grid()
+    
+def coo_box(head,region):
+    '''
+    get box pixel coordinates 
+    l1,l2,b1,b2 = region
+    '''
+    l1,l2,b1,b2 = region
+    w = wcs.WCS(head)
+    if head['NAXIS'] == 2:
+        x1,y1 = w.wcs_world2pix(l2,b1,0)
+        x2,y2 = w.wcs_world2pix(l1,b2,0)   
+    if head['NAXIS'] == 3:
+        x1,y1,v = w.wcs_world2pix(l2,b1,0,0)
+        x2,y2,v = w.wcs_world2pix(l1,b2,0,0)
+    if head['NAXIS'] == 4:
+        x1,y1,v,s = w.wcs_world2pix(l2,b1,0,0,0)
+        x2,y2,v,s = w.wcs_world2pix(l1,b2,0,0,0)
+    return x1,y1,x2,y2
+        
+def coo_circle(head,region):
+    '''
+    get circle pixel coordinates
+    l,b,r = region
+    please gurantee the head['CDELT1']=head['CDELT2']
+    '''
+    l,b,r = region
+    r = r/np.abs(head['CDELT1'])
+    w = wcs.WCS(head)
+    if head['NAXIS'] == 2:
+        x,y = w.wcs_world2pix(l,b,0)
+    if head['NAXIS'] == 3:
+        x,y,v = w.wcs_world2pix(l,b,0,0)
+    if head['NAXIS'] == 4:
+        x,y,v,s = w.wcs_world2pix(l,b,0,0,0)
+    return x,y,r
+
+ 
 
