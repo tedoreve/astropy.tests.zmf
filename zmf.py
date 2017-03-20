76edb169931e959c5f289510f0ba867b1afa37b5
tedoreve <wlzzs666@gmail.com>
2017-03-20 21:07:09 +0800

    auto commit

--- zmf.py
+++ zmf.py
@@ -7,8 +7,10 @@ Created on Mon Jul  4 17:49:54 2016
 
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.path as mpath
 from astropy import units as u
 from astropy import wcs
+import copy as cp
 
 def distance2diameter(distance,angle):
     '''distance(kpc) angle(arcmin) return(pc)'''
@@ -26,11 +28,11 @@ def conversion(v,bmaj,bmin):
     equiv = u.brightness_temperature(beam_area, freq)
     return u.Jy.to(u.K, equivalencies=equiv)  
 
-def plot_fits(data,head,contrast,name):
+def plot_2Dfits(data,head,contrast):
     '''
     plot original continuum figure
     contrast is the contrast ratio, it would be better to use 1.
-    name is the title of the image
+    Be careful!!! The nan values will be set to zeros.
     '''
     
     w = wcs.WCS(head)
@@ -48,7 +50,6 @@ def plot_fits(data,head,contrast,name):
     result = (result-np.mean(result))+np.mean(result)*contrast
 
     plt.subplots() 
-    plt.title(name)
     plt.imshow(np.log(result),origin='lower',interpolation='nearest',extent=[l1,l2,b1,b2])
     plt.grid()
     
@@ -86,6 +87,101 @@ def coo_circle(head,region):
     if head['NAXIS'] == 4:
         x,y,v,s = w.wcs_world2pix(l,b,0,0,0)
     return x,y,r
+
+def velocity(data,head):
+    '''
+    return spec velocity axis
+    '''
+    w   = wcs.WCS(head)
+    pix = np.linspace(1,data.shape[0],data.shape[0])
+    if head['NAXIS'] == 3:
+        x,y,v   = w.wcs_pix2world(0,0,pix,0)
+    if head['NAXIS'] == 4:
+        x,y,v,s   = w.wcs_pix2world(0,0,pix,0,0)
+    return v
+
+def box(data,head,contrast,region,onoff,*args):
+    '''
+    plot box pixel coordinates
+    '''
+    l1,l2,b1,b2 = region  
+    x1,y1,x2,y2 = coo_box(head,region)
+    if data.ndim == 2:
+        result0 = data[y1:y2,x1:x2]
+        result1 = result0
+    if data.ndim == 3:
+        result0 = data[:,y1:y2,x1:x2] 
+        result1 = data[args[0],y1:y2,x1:x2] 
+    result1 = (result1-np.mean(result1))+np.mean(result1)*contrast
+    
+    plt.subplots() 
+    plt.imshow(result1,origin='lower',interpolation='nearest',extent=[l2,l1,b1,b2])
+    plt.colorbar()   
+    
+    Path = mpath.Path
+    path_data = [
+        (Path.MOVETO, (onoff[0], onoff[2])),
+        (Path.CURVE4, (onoff[0], onoff[3])),
+        (Path.CURVE4, (onoff[1], onoff[3])),
+        (Path.CURVE4, (onoff[1], onoff[2])),
+        (Path.CLOSEPOLY, (onoff[0], onoff[2])),   
+        ]
+    codes, verts = zip(*path_data)
+    path = Path(verts, codes)
+    x, y = zip(*path.vertices)
+    plt.plot(x, y, 'go-')
+    plt.xlim(l2,l1)
+    plt.ylim(b1,b2)
+    plt.grid()
+    
+    x1,y1,x2,y2 = coo_box(head,onoff)  
+    if data.ndim == 2:
+        result2 = data[y1:y2,x1:x2]
+    if data.ndim == 3:
+        result2 = data[:,y1:y2,x1:x2]
+        
+    return result2,result0
+
+
+def circle(data,head,contrast,region,onoff,*args):
+    '''
+    plot circle pixel coordinates
+    '''
+    l1,l2,b1,b2 = region  
+    x1,y1,x2,y2 = coo_box(head,region)
+    if data.ndim == 2:
+        result0 = data[y1:y2,x1:x2]
+        result1 = result0
+    if data.ndim == 3:
+        result0 = data[:,y1:y2,x1:x2] 
+        result1 = data[args[0],y1:y2,x1:x2] 
+    result0 = (result0-np.mean(result0))+np.mean(result0)*contrast
+    
+    plt.subplots()
+    plt.imshow(result1,origin='lower',interpolation='nearest',extent=[l2,l1,b1,b2])
+    plt.colorbar()
+    
+    an = np.linspace(0, 2*np.pi, 100)
+    plt.plot(onoff[2]*np.cos(an)+onoff[0], onoff[2]*np.sin(an)+onoff[1],'r')
+    plt.xlim(l2,l1)
+    plt.ylim(b1,b2)
+    plt.grid()
+    
+    x,y,r =coo_circle(head,onoff)
+    if data.ndim == 2:
+        result2 = cp.copy(data[int(y-r):int(y+r),int(x-r):int(x+r)])
+        for i in range(result2.shape[0]):
+            for j in range(result2.shape[1]):
+                if (i-result2.shape[0]/2)**2+(j-result2.shape[1]/2)**2 > r**2:
+                    result2[i,j]=0
+    if data.ndim == 3:
+        result2 = cp.copy(data[:,int(y-r):int(y+r),int(x-r):int(x+r)])
+        for i in range(result2.shape[1]):
+            for j in range(result2.shape[2]):
+                if (i-result2.shape[1]/2)**2+(j-result2.shape[2]/2)**2 > r**2:
+                    result2[:,i,j]=0
+                           
+    return result2,result0
 
  
 
