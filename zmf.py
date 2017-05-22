a6692edd58c677ac252fc1577653378ae5f2ebd4
tedoreve <wlzzs666@gmail.com>
2017-05-22 19:16:19 +0800

    auto commit

--- zmf.py
+++ zmf.py
@@ -23,12 +23,18 @@ def conversion(v,bmaj,bmin):
     '''
     v(GHz),bmaj(deg),bmin(deg),retrun(Jy/beam -> K)
     '''
-    bmaj = bmaj*u.deg
-    bmin = bmin*u.deg
-    fwhm_to_sigma = 1./(8*np.log(2))**0.5
-    beam_area = 2.*np.pi*(bmaj*bmin*fwhm_to_sigma**2)
-    freq = v*u.GHz
-    equiv = u.brightness_temperature(beam_area, freq)
+    if bmaj == bmin:
+        beam_sigma = bmaj*u.deg
+        beam_area = 2*np.pi*(beam_sigma)**2
+        freq = v*u.GHz
+        equiv = u.brightness_temperature(beam_area, freq)
+    else:
+        bmaj = bmaj*u.deg
+        bmin = bmin*u.deg
+        fwhm_to_sigma = 1./(8*np.log(2))**0.5
+        beam_area = 2.*np.pi*(bmaj*bmin*fwhm_to_sigma**2)
+        freq = v*u.GHz
+        equiv = u.brightness_temperature(beam_area, freq)
     return u.Jy.to(u.K, equivalencies=equiv)  
 
 def dist(l,b,d,V = 220,v_sun = 220,r_sun = 8.5):
